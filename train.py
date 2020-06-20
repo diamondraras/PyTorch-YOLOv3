b6b32d44d4c72b4d1cee4dc268e0cfc2f9f1d47b
Rasoamanana Diamondra <diamondraras@gmail.com>
2020-06-20 21:31:39 +0300

    Update train.py

--- train.py
+++ train.py
@@ -167,11 +167,11 @@ if __name__ == "__main__":
             # logger.list_of_scalars_summary(evaluation_metrics, epoch)
 
             # Print class APs and mAP
-            ap_table = [["Index", "Class name", "AP"]]
-            for i, c in enumerate(ap_class):
-                ap_table += [[c, class_names[c], "%.5f" % AP[i]]]
-            print(AsciiTable(ap_table).table)
-            print(f"---- mAP {AP.mean()}")
+            # ap_table = [["Index", "Class name", "AP"]]
+            # for i, c in enumerate(ap_class):
+            #     ap_table += [[c, class_names[c], "%.5f" % AP[i]]]
+            # print(AsciiTable(ap_table).table)
+            # print(f"---- mAP {AP.mean()}")
 
         if epoch % opt.checkpoint_interval == 0:
             torch.save(model.state_dict(), f"checkpoints/yolov3_ckpt_%d.pth" % epoch)
